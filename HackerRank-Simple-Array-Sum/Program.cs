2fef1f2e0fa15cc8c52c547fed1b68f2f917ade8
jaboud <jaboud94@gmail.com>
2019-09-06 10:45:04 +1000

    Initial solution creation.

---
+++ HackerRank-Simple-Array-Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_Simple_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
