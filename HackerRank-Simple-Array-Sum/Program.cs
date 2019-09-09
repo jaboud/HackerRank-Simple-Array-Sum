906bdf3e6d9af4e87c03dbfedd318fd5d7799485
jaboud <jaboud94@gmail.com>
2019-09-09 10:56:52 +1000

    Declaration of Integer and converting it to a Int32 type.

--- HackerRank-Simple-Array-Sum/Program.cs
+++ HackerRank-Simple-Array-Sum/Program.cs
@@ -6,6 +6,9 @@ namespace HackerRank_Simple_Array_Sum
     {
         static void Main(string[] args)
         {
+            //Declaration of Integer and converting it to a Int32 type.
+            int n = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Hello World!");
         }
     }
